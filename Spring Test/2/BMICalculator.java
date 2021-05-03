ecc6fcfff253c5a6612e84c6567e08ef2d768332
lmanesh7 <47899760+lmanesh7@users.noreply.github.com>
2021-05-03 19:53:38 +0530

    Update BMICalculator.java

--- Spring Test/2/BMICalculator.java
+++ Spring Test/2/BMICalculator.java
@@ -1,23 +1,32 @@
 package com;
 
 public class BMICalculator {
-private Patient patient;
+private Patient patient[];
 
-public Patient getPatient() {
+public Patient[] getPatient() {
 	return patient;
 }
 
-public void setPatient(Patient patient) {
+public void setPatient(Patient[] patient) {
 	this.patient = patient;
 }
-
-public double BMI()
-{
-	return (patient.getPatientWeight()/patient.getPatientHeight()*patient.getPatientHeight())*703;
+double calculateBMI(double patientWeight, double patientHeight) {
+	double bmi;
+	bmi=(patientWeight/(patientHeight*patientHeight))*703;
+	if(bmi>25.0) {
+		System.out.println(bmi);
+	}
+	else {
+		System.out.println("Normal");
+	}
+	return 0;
 }
 
+public BMICalculator() {
+	super();
+	//System.out.println("inside bmic");
+}
 
-	
 
 
 }
